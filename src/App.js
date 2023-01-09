e5fcd20d6006e354cbd9aafe1b534058fa24f619
nurbekovv05 <baevnurbol@gmail.com>
2023-01-09 14:11:58 +0600

    command

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import Home from "./components/Home/Home"
+import School from "./components/School/School"
+import About from "./components/About/About"
+import Follow from "./components/Follow/Follow"
+import Sing from "./components/Sing/Sing-in"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sing/>
+      <Follow/>
+      <About/>
+      <Home/>
+      <School/>
+
     </div>
   );
 }
